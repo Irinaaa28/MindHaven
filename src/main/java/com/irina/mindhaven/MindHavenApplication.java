29ca66211fc11b10e7fb6cef168141e90868e3ad
Irinaaa28 <comanirina0128@gmail.com>
2025-08-20 13:44:21 +0300

    First commit!

---
+++ src/main/java/com/irina/mindhaven/MindHavenApplication.java
@@ -0,0 +1,13 @@
+package com.irina.mindhaven;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MindHavenApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MindHavenApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/irina/mindhaven/MindHavenApplicationTests.java
@@ -0,0 +1,13 @@
+package com.irina.mindhaven;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MindHavenApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
